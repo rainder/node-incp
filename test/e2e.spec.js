4a428f03850287d4bfc86f64e40dee83f0410902
Andrius Skerla <andrius@skerla.com>
2016-08-30 10:49:28 +0100

    world

--- test/e2e.spec.js
+++ test/e2e.spec.js
@@ -1,346 +1,256 @@
 'use strict';
 
 require('co-mocha');
+const tryCatch = require('co-try-catch');
 const chai = require('chai');
 const INCP = require('./../lib/incp');
+const INCPError = require('./../lib/incp-error');
 
 chai.should();
 
 describe('work just fine', function () {
 
-  // it('should use loopback interface', function *() {
-  //   const incp1 = new INCP({
-  //     type: 'one'
-  //   });
+  it('should connect to the node', function *() {
+    const incp1 = new INCP();
+    const incp2 = new INCP();
+
+    yield [
+      cb => incp1.once('ready', cb),
+      cb => incp2.once('ready', cb)
+    ];
+
+    yield incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port);
+
+    (yield [
+      cb => incp1.getServer()._server.getConnections(cb),
+      cb => incp2.getServer()._server.getConnections(cb)
+    ]).should.deep.equals([0, 1]);
+  });
+
+  it('should return node if connecting to the same node', function *() {
+    const incp1 = new INCP();
+    const incp2 = new INCP();
+
+    yield [
+      cb => incp1.once('ready', cb),
+      cb => incp2.once('ready', cb)
+    ];
+
+    const node1 = yield incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port);
+    const node2 = yield incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port);
+
+    node1.should.equals(node2);
+
+    yield cb => setTimeout(cb, 100);
+
+    (yield [
+      cb => incp1.getServer()._server.getConnections(cb),
+      cb => incp2.getServer()._server.getConnections(cb)
+    ]).should.deep.equals([0, 1]);
+  });
+
+  it('should not create 2x duplex connections', function *() {
+    const incp1 = new INCP();
+    const incp2 = new INCP();
+
+    yield [
+      cb => incp1.once('ready', cb),
+      cb => incp2.once('ready', cb)
+    ];
+
+    yield incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port);
+
+    (yield [
+      cb => incp1.getServer()._server.getConnections(cb),
+      cb => incp2.getServer()._server.getConnections(cb)
+    ]).should.deep.equals([0, 1]);
+
+    try {
+      yield incp2.connectTo(incp1.config.getOptions().host, incp1.config.getOptions().port);
+      false.should.equals(true);
+    } catch (e) {
+
+    }
+
+    yield cb => setTimeout(cb, 100);
+
+    // console.log(incp1.getNodes());
+    // console.log(incp2.getNodes());
+
+    (yield [
+      cb => incp1.getServer()._server.getConnections(cb),
+      cb => incp2.getServer()._server.getConnections(cb)
+    ]).should.deep.equals([0, 1]);
+
+  });
+
+  it('should not create 2x duplex connections (race condition)', function *() {
+    const incp1 = new INCP();
+    const incp2 = new INCP();
+
+    yield [
+      cb => incp1.once('ready', cb),
+      cb => incp2.once('ready', cb)
+    ];
+
+    const nodes = yield tryCatch([
+      incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port),
+      incp2.connectTo(incp1.config.getOptions().host, incp1.config.getOptions().port)
+    ]);
+
+    yield cb => setTimeout(cb, 100);
+
+    // console.log(incp1.getNodes());
+    // console.log(incp2.getNodes());
+
+    (yield [
+      cb => incp1.getServer()._server.getConnections(cb),
+      cb => incp2.getServer()._server.getConnections(cb)
+    ]).should.deep.equals([0, 1]);
+  });
+
+  it('should respect groups', function *() {
+    const incp1 = new INCP({ group: 'one' });
+    const incp2 = new INCP({ group: 'two' });
+
+    yield [
+      cb => incp1.once('ready', cb),
+      cb => incp2.once('ready', cb)
+    ];
+
+    try {
+      yield incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port);
+    } catch (e) {
+      e.errno.should.equals(INCPError.GROUP_MISMATCH.errno());
+    }
+
+    yield cb => setTimeout(cb, 100);
+
+    (yield [
+      cb => incp1.getServer()._server.getConnections(cb),
+      cb => incp2.getServer()._server.getConnections(cb)
+    ]).should.deep.equals([0, 0]);
+  });
+
+  it('should introduce relatives', function *() {
+    const incp1 = new INCP();
+    const incp2 = new INCP();
+    const incp3 = new INCP();
+
+    yield [
+      cb => incp1.once('ready', cb),
+      cb => incp2.once('ready', cb),
+      cb => incp3.once('ready', cb),
+    ];
+
+    yield incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port);
+    yield incp2.connectTo(incp3.config.getOptions().host, incp3.config.getOptions().port);
+
+    yield cb => setTimeout(cb, 500);
+
+    Array.from(incp1.getNodes().keys()).should.deep.equals([
+      incp2.getId(),
+      incp3.getId(),
+    ]);
+
+    Array.from(incp2.getNodes().keys()).should.deep.equals([
+      incp1.getId(),
+      incp3.getId(),
+    ]);
+
+    Array.from(incp3.getNodes().keys()).should.deep.equals([
+      incp2.getId(),
+      incp1.getId(),
+    ]);
+  });
+
+  it('should send external request', function *() {
+    const incp1 = new INCP();
+    const incp2 = new INCP();
+
+    yield [
+      cb => incp1.once('ready', cb),
+      cb => incp2.once('ready', cb),
+    ];
+
+    const node = yield incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port);
+
+    incp2.onRequest = function *(msg) {
+      return msg.asd;
+    };
+
+    const response = yield node.request({
+      asd: 34
+    });
+
+    response.should.equals(34);
+  });
+
+  it('should spam with messages', function *() {
+    const incp1 = new INCP();
+    const incp2 = new INCP();
+
+    yield [
+      cb => incp1.once('ready', cb),
+      cb => incp2.once('ready', cb),
+    ];
+
+    const node = yield incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port);
+
+    incp2.onRequest = function *(msg) {
+      return msg.asd + 1;
+    };
+
+    const responses = yield Array.from(new Array(32)).map((a, index) => node.request({
+      asd: index
+    }));
+
+    responses.should.deep.equals(Array.from(new Array(32)).map((_, index) => index + 1));
+  });
+
+  it('should destroy client the connection and work just fine', function *() {
+    const incp1 = new INCP();
+    const incp2 = new INCP();
+
+    yield [
+      cb => incp1.once('ready', cb),
+      cb => incp2.once('ready', cb),
+    ];
+
+    const node = yield incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port);
+
+    incp2.onRequest = function *(msg) {
+      return msg.asd;
+    };
+
+    incp2.getNodes().get(incp1.getId()).getSocket().end();
+    yield cb => setTimeout(cb, 100);
+
+    const response1 = yield tryCatch(node.request({
+      asd: 34
+    }));
+  });
+
+  // it('should destroy server the connection and work just fine', function *() {
+  //   const incp1 = new INCP();
+  //   const incp2 = new INCP();
   //
-  //   yield incp1.start();
+  //   yield [
+  //     cb => incp1.once('ready', cb),
+  //     cb => incp2.once('ready', cb),
+  //   ];
   //
-  //   incp1.setMessageHandler((message, socket) => {
-  //     return { a: 6 };
-  //   });
+  //   const node = yield incp1.connectTo(incp2.config.getOptions().host, incp2.config.getOptions().port);
   //
-  //   const response = yield incp1.sendToNode(incp1.id, { test: true });
+  //   incp2.onRequest = function *(msg) {
+  //     incp2.getNodes().get(incp1.getId()).getSocket().end();
+  //     return msg.asd;
+  //   };
   //
-  //   response.a.should.equals(6);
+  //   yield cb => setTimeout(cb, 500);
+  //
+  //   const response1 = yield tryCatch(node.request({
+  //     asd: 34
+  //   }));
   // });
-
-  it('should reverse-connect to the node', function *() {
-    const incp1 = new INCP({
-      type: 'one'
-    });
-    const incp2 = new INCP({
-      type: 'two'
-    });
-
-    yield incp1.start();
-    yield incp2.start();
-
-    yield incp1.connectTo({
-      host: incp2.server.host,
-      port: incp2.server.port
-    });
-
-    yield cb => setTimeout(cb, 100);
-
-    const serverConnections = yield [
-      cb => incp1.server.getServer().getConnections(cb),
-      cb => incp2.server.getServer().getConnections(cb)
-    ];
-
-    serverConnections.should.deep.equals([1, 1]);
-  });
-
-  it('should send a message to the node', function *() {
-    const incp1 = new INCP({
-      type: 'one'
-    });
-    const incp2 = new INCP({
-      type: 'two'
-    });
-
-    yield incp1.start();
-    yield incp2.start();
-
-    yield incp1.connectTo({
-      host: incp2.server.host,
-      port: incp2.server.port
-    });
-
-    incp2.setMessageHandler(function *(data) {
-      return { ehllo: true };
-    });
-
-    const response = yield incp1.sendToNode(incp2.id, {
-      hello: true
-    });
-
-    response.should.have.keys(['ehllo']);
-  });
-
-  it('should ignore same connection to the node', function *() {
-    const incp1 = new INCP({
-      type: 'one'
-    });
-    const incp2 = new INCP({
-      type: 'two'
-    });
-
-    yield incp1.start();
-    yield incp2.start();
-
-    yield [
-      incp1.connectTo({
-        host: incp2.server.host,
-        port: incp2.server.port
-      }),
-      incp1.connectTo({
-        host: incp2.server.host,
-        port: incp2.server.port
-      }),
-      incp2.connectTo({
-        host: incp1.server.host,
-        port: incp1.server.port
-      })
-    ];
-
-    yield cb => setTimeout(cb, 100);
-
-    const serverConnections = yield [
-      cb => incp1.server.getServer().getConnections(cb),
-      cb => incp2.server.getServer().getConnections(cb)
-    ];
-
-    serverConnections.should.deep.equals([1, 1]);
-  });
-
-  it('should suggest the other nodes', function *() {
-    const incp1 = new INCP({
-      type: 'one'
-    });
-    const incp2 = new INCP({
-      type: 'two'
-    });
-    const incp3 = new INCP({
-      type: 'three'
-    });
-
-    yield incp1.start();
-    yield incp2.start();
-    yield incp3.start();
-
-    yield [
-      incp1.connectTo({
-        host: incp2.server.host,
-        port: incp2.server.port
-      }),
-      incp2.connectTo({
-        host: incp1.server.host,
-        port: incp1.server.port
-      })
-    ];
-
-    yield cb => setTimeout(cb, 100);
-
-    {
-      const serverConnections = yield [
-        cb => incp1.server.getServer().getConnections(cb),
-        cb => incp2.server.getServer().getConnections(cb),
-        cb => incp3.server.getServer().getConnections(cb)
-      ];
-
-      serverConnections.should.deep.equals([1, 1, 0]);
-    }
-
-    yield incp3.connectTo({
-      host: incp1.server.host,
-      port: incp1.server.port
-    });
-
-    yield cb => setTimeout(cb, 100);
-
-    {
-      // console.log(incp1.id, incp1.nodes.keys());
-      // console.log(incp2.id, incp2.nodes.keys());
-      // console.log(incp3.id, incp3.nodes.keys());
-
-      const serverConnections = yield [
-        cb => incp1.server.getServer().getConnections(cb),
-        cb => incp2.server.getServer().getConnections(cb),
-        cb => incp3.server.getServer().getConnections(cb)
-      ];
-
-      serverConnections.should.deep.equals([2, 2, 2]);
-    }
-  });
-
-  it('should shutdown a node', function *() {
-    const incp1 = new INCP({
-      type: 'one'
-    });
-    const incp2 = new INCP({
-      type: 'two'
-    });
-    const incp3 = new INCP({
-      type: 'three'
-    });
-
-    yield incp1.start();
-    yield incp2.start();
-    yield incp3.start();
-
-    yield [
-      incp1.connectTo({
-        host: incp2.server.host,
-        port: incp2.server.port
-      }),
-      incp2.connectTo({
-        host: incp1.server.host,
-        port: incp1.server.port
-      }),
-      incp3.connectTo({
-        host: incp1.server.host,
-        port: incp1.server.port
-      })
-    ];
-
-    yield cb => setTimeout(cb, 100);
-
-    {
-      const serverConnections = yield [
-        cb => incp1.server.getServer().getConnections(cb),
-        cb => incp2.server.getServer().getConnections(cb),
-        cb => incp3.server.getServer().getConnections(cb)
-      ];
-
-      serverConnections.should.deep.equals([2, 2, 2]);
-    }
-
-    yield incp3.shutdown();
-
-    yield cb => setTimeout(cb, 100);
-
-    {
-      const serverConnections = yield [
-        cb => incp1.server.getServer().getConnections(cb),
-        cb => incp2.server.getServer().getConnections(cb),
-        cb => incp3.server.getServer().getConnections(cb)
-      ];
-
-      serverConnections.should.deep.equals([1, 1, 0]);
-    }
-  });
-
-  it('should ignore different groups1', function *() {
-    const incp1 = new INCP({
-      type: 'one',
-      group: 'house'
-    });
-    const incp2 = new INCP({
-      type: 'two',
-      group: 'car'
-    });
-
-    yield incp1.start();
-    yield incp2.start();
-
-    try {
-      yield incp1.connectTo({
-        host: incp2.server.host,
-        port: incp2.server.port
-      });
-      false.should.equals(true);
-    } catch (e) {
-      e.errno.should.equals(4)
-    }
-
-    yield cb => setTimeout(cb, 100);
-    incp1.nodes.size.should.equals(0);
-    incp2.nodes.size.should.equals(0);
-  });
-
-  it('should ignore different groups2', function *() {
-    const incp1 = new INCP({
-      type: 'one'
-    });
-    const incp2 = new INCP({
-      type: 'two',
-      group: 'car'
-    });
-
-    yield incp1.start();
-    yield incp2.start();
-
-    try {
-      yield incp1.connectTo({
-        host: incp2.server.host,
-        port: incp2.server.port
-      });
-      false.should.equals(true);
-    } catch (e) {
-      e.errno.should.equals(4)
-    }
-
-    yield cb => setTimeout(cb, 100);
-    incp1.nodes.size.should.equals(0);
-    incp2.nodes.size.should.equals(0);
-  });
-
-  it('should ignore different groups3', function *() {
-    const incp1 = new INCP({
-      type: 'one',
-      group: 'car'
-    });
-    const incp2 = new INCP({
-      type: 'two'
-    });
-
-    yield incp1.start();
-    yield incp2.start();
-
-    try {
-      yield incp1.connectTo({
-        host: incp2.server.host,
-        port: incp2.server.port
-      });
-      false.should.equals(true);
-    } catch (e) {
-      e.errno.should.equals(4)
-    }
-
-    yield cb => setTimeout(cb, 100);
-    incp1.nodes.size.should.equals(0);
-    incp2.nodes.size.should.equals(0);
-  });
-
-  it('should set a flag on a node', function *() {
-    const incp1 = new INCP({
-      type: 'one'
-    });
-    const incp2 = new INCP({
-      type: 'two',
-      flags: {
-        test: 4
-      }
-    });
-
-    yield incp1.start();
-    yield incp2.start();
-
-    yield incp1.connectTo({
-      host: incp2.server.host,
-      port: incp2.server.port
-    });
-
-    yield incp1.flag('state', 'quiting');
-
-    yield cb => setTimeout(cb, 100);
-    incp1.nodes.size.should.equals(1);
-    incp2.nodes.size.should.equals(1);
-
-    incp1.config.flags.state.should.equals('quiting');
-    incp2.nodes.get(incp1.getId()).options.remote.flags.state.should.equals('quiting');
-
-    incp2.config.flags.test.should.equals(4);
-    incp1.nodes.get(incp2.getId()).options.remote.flags.test.should.equals(4);
-  });
 });
