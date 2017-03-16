3f9c0bfc6f1d6ce6b03b949cd6d3d1077dd58607
Andrius Skerla <rainder@gmail.com>
2017-03-16 09:17:33 +0000

    async-await

--- test/connection.spec.js
+++ test/connection.spec.js
@@ -1,6 +1,5 @@
 'use strict';
 
-const co = require('co');
 const net = require('net');
 const INCP = require('./..');
 require('chai').should();
@@ -8,45 +7,45 @@ require('chai').should();
 describe('connection', function () {
   this.timeout(2000);
 
-  it('should create servers', co.wrap(function *() {
+  it('should create servers', async function () {
     const incp1 = new INCP();
     const incp2 = new INCP();
 
-    const r = yield [
+    const r = await Promise.all([
       incp1.startServer(),
       incp2.startServer(),
-    ];
+    ]);
 
     r[0].address.should.equals('0.0.0.0');
     r[0].family.should.equals('IPv4');
     r[0].port.should.gt(0);
 
-    yield [
+    await Promise.all([
       incp1.shutdown(),
       incp2.shutdown(),
-    ];
-  }));
+    ]);
+  });
 
-  it('should fail to connect to the server', co.wrap(function *() {
+  it('should fail to connect to the server', async function () {
     const incp1 = new INCP();
     const incp2 = new INCP();
 
-    yield [
+    await Promise.all([
       incp1.startServer(),
       incp2.startServer(),
-    ];
+    ]);
 
-    const r = yield incp1.connectTo(38986, null, { max_retries_count: 0 }).catch(() => true);
+    const r = await incp1.connectTo(38986, null, { max_retries_count: 0 }).catch(() => true);
 
     r.should.equals(true);
 
-    yield [
+    await Promise.all([
       incp1.shutdown(),
       incp2.shutdown(),
-    ];
-  }));
+    ]);
+  });
 
-  it('should connect to the server', co.wrap(function *() {
+  it('should connect to the server', async function () {
     const incp1 = new INCP({
       id: 2,
     });
@@ -54,99 +53,103 @@ describe('connection', function () {
       id: 1,
     });
 
-    yield [
+    await Promise.all([
       incp1.startServer(),
       incp2.startServer(),
-    ];
+    ]);
 
-    yield incp1.connectTo(incp2.getConfiguration().getPort());
+    await incp1.connectTo(incp2.getConfiguration().getPort());
 
-    yield cb => setTimeout(cb, 10);
+    await new Promise((cb) => setTimeout(cb, 10));
 
-    yield [
+    await Promise.all([
       incp1.shutdown(),
       incp2.shutdown(),
-    ];
-  }));
+    ]);
+  });
 
-  it('should send invalid proto', co.wrap(function *() {
+  it('should send invalid proto', async function () {
     const incp = new INCP();
 
-    yield incp.startServer();
+    await incp.startServer();
 
     const socket = net.connect(incp.getConfiguration().getPort());
 
-    yield cb => socket.once('connect', cb);
-
-    yield cb => setTimeout(cb, 100);
-
-    yield incp.getServer().getConnections().then((c) => c.should.equals(1));
-
-    const closePromise = co(function *() {
-      yield cb => socket.once('close', cb);
+    await new Promise((resolve, reject) => {
+      socket.once('connect', (err) => {
+        err ? reject(err) : resolve();
+      });
     });
 
-    const err = yield cb => {
+    await new Promise((cb) => setTimeout(cb, 100));
+
+    await incp.getServer().getConnections().then((c) => c.should.equals(1));
+
+    const closePromise = new Promise((resolve, reject) => {
+      socket.once('close', (err) => err ? reject(err) : resolve());
+    });
+
+    const err = await new Promise((resolve) => {
       incp.on('error', (err) => {
-        cb(null, err);
+        resolve(err);
       });
 
       socket.write('hello');
-    };
+    });
 
     err.errno.should.equals(INCP.Error.INVALID_PROTO.errno());
 
-    yield cb => setTimeout(cb, 100);
+    await new Promise((cb) => setTimeout(cb, 100));
 
     //sever should have no connections
-    yield incp.getServer().getConnections().then((c) => c.should.equals(0));
+    await incp.getServer().getConnections().then((c) => c.should.equals(0));
 
     //client should get close event
-    yield closePromise;
+    await closePromise;
 
-    yield cb => setTimeout(cb, 10);
+    await new Promise((cb) => setTimeout(cb, 10));
 
-    yield incp.shutdown();
-  }));
+    await incp.shutdown();
+  });
 
-  it('should test concurrent connection', co.wrap(function *() {
+  it('should test concurrent connection', async function () {
     const incp1 = new INCP({ id: 2 });
     const incp2 = new INCP({ id: 1 });
 
-    yield [
+    await Promise.all([
       incp1.startServer(),
       incp2.startServer(),
-    ];
+    ]);
 
-    const nodes = yield [
+    const nodes = await Promise.all([
       incp1.connectTo(incp2.getConfiguration().getPort()).catch(e => null),
       incp2.connectTo(incp1.getConfiguration().getPort()).catch(e => null)
-    ];
+    ]);
 
-    yield cb => setTimeout(cb, 100);
+    await new Promise((cb) => setTimeout(cb, 100));
 
     incp1.getNodes().size.should.equals(1);
     incp2.getNodes().size.should.equals(1);
 
-    yield [
+    await Promise.all([
       incp1.shutdown(),
       incp2.shutdown(),
-    ];
-  }));
+    ]);
+  });
 
-  it('should reassign connection ownership', co.wrap(function *() {
+  it('should reassign connection ownership', async function () {
     const incp1 = new INCP({
       id: 1,
     });
     const incp2 = new INCP({
       id: 2,
     });
-    yield [incp1.startServer(), incp2.startServer()];
+    await Promise.all([incp1.startServer(), incp2.startServer()]);
 
-    yield incp1.connectTo(incp2.getConfiguration().getPort()).then(() => true.should.equals(false), () => true);
+    await incp1.connectTo(incp2.getConfiguration().getPort()).then(() => true.should.equals(false), () => true);
 
     //wait until reconnects
-    yield cb => setTimeout(cb, 100);
+    await new Promise((cb) => setTimeout(cb, 100));
 
     // console.dir([
     //   incp1.getConfiguration(),
@@ -162,45 +165,45 @@ describe('connection', function () {
     Array.from(incp2.getNodes().values())[0].getConfiguration().getId()
       .should.equals(incp1.getConfiguration().getId());
 
-    yield [
+    await Promise.all([
       incp1.shutdown(),
       incp2.shutdown(),
-    ];
-  }));
+    ]);
+  });
 
-  it('should send request', co.wrap(function *() {
+  it('should send request', async function () {
     const incp1 = new INCP({ id: 2 });
     const incp2 = new INCP({ id: 1 });
-    yield [incp1.startServer(), incp2.startServer()];
+    await Promise.all([incp1.startServer(), incp2.startServer()]);
 
-    yield incp1.connectTo(incp2.getConfiguration().getPort());
+    await incp1.connectTo(incp2.getConfiguration().getPort());
 
-    incp2.onRequest = function *({ a }) {
+    incp2.onRequest = async({ a }) => {
       return a + 5;
     };
 
-    const r = yield Array.from(incp1.getNodes().values())[0].request({ a: 5 });
+    const r = await Array.from(incp1.getNodes().values())[0].request({ a: 5 });
 
     r.should.equals(10);
-  }));
+  });
 
-  it('should send push', co.wrap(function *() {
+  it('should send push', async function () {
     const incp1 = new INCP({ id: 2 });
     const incp2 = new INCP({ id: 1 });
-    yield [incp1.startServer(), incp2.startServer()];
+    await Promise.all([incp1.startServer(), incp2.startServer()]);
 
-    yield incp1.connectTo(incp2.getConfiguration().getPort());
+    await incp1.connectTo(incp2.getConfiguration().getPort());
 
     let r = 0;
-    incp2.onPush = function *({ a }) {
+    incp2.onPush = async({ a }) => {
       r += a;
     };
 
-    yield Array.from(incp1.getNodes().values())[0].push({ a: 5 });
+    await Array.from(incp1.getNodes().values())[0].push({ a: 5 });
 
-    yield cb => setTimeout(cb, 10);
+    await new Promise((cb) => setTimeout(cb, 10));
     r.should.equals(5);
-  }));
+  });
 
 
 });
